626605de0683815c8dee5c32140e3e71594e6888
Yousef-Alsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2022-10-26 02:22:25 +0300

    Update ContactUs.blade.php

--- resources/views/ContactUs.blade.php
+++ resources/views/ContactUs.blade.php
@@ -12,6 +12,119 @@
     @include('includes.header', ['mainTitle' => 'Contact ', 'subTitle' => 'US'])
 
     <div class="text-center">CONTACT US TEMP</div>
+    <div class="flex flex-col">
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="overflow-hidden">
+          <table class="min-w-full text-center">
+            <thead class="border-b">
+              <tr>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 ">
+                  Name
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                  Employer ID
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                  Email
+                </th>
+                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                  Social
+              </tr>
+            </thead>
+            <tbody>
+
+              <tr class="border-b">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Abdulaziz Bawazer
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXXXXX
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXX@stu.kau.edu.sa
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  
+                </td>
+              </tr>
+              <tr class="border-b">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Abdulrahman Hakami
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXXXXX
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXX@stu.kau.edu.sa
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  
+                </td>
+              </tr>
+              <tr class="border-b">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Ahmad Alzahrani
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXXXXX
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXX@stu.kau.edu.sa
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  
+                </td>
+              </tr>
+              <tr class="border-b">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Ali Habibullah
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXXXXX
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXX@stu.kau.edu.sa
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  
+                </td>
+              </tr>
+              <tr class="border-b">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Marwan Aljedaani
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXXXXX
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  XXXX@stu.kau.edu.sa
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  
+                </td>
+              </tr>
+
+              <tr class="border-b">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Yousef Alsyaegh 
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  1938256
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  yalsayegh@stu.kau.edu.sa
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                </td>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
     @include('includes.footer')
 </body>
 
